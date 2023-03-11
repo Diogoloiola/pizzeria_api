8b5e88fa3c98260691cb4ce20971b5f7b0f4c8d9
Diogo Loiola <diogomouraloiola@gmail.com>
2023-03-11 10:43:15 -0300

    feat: atualizando o seeds do projeto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Ingredient.destroy_all
-Pizza.destroy_all
 PromotionalCode.destroy_all
+Pizza.destroy_all
+Seller.destroy_all
+Discount.destroy_all
+Order.destroy_all
 
 ingredients = [
   { name: 'Onions', multiplier: 1 },
@@ -27,3 +30,22 @@ Pizza.find_each do |pizza|
 end
 
 PromotionalCode.create!(code: SecureRandom.uuid.slice(0, 5), from: 2, to: 1, target_id: Pizza.last.id, size: 1)
+
+Seller.create(name: Faker::Name.name,
+              email: Faker::Internet.email,
+              cpf: CPF.new(CPF.generate).formatted,
+              profile: %w[admin functionarty].sample,
+              password: Faker::Number.number(digits: 10).to_s)
+
+Discount.create(deduction_in_percent: 5, code: SecureRandom.uuid.slice(0, 5))
+
+order = Order.new(
+  discount_code: Discount.last.id,
+  seller_id: Seller.last.id,
+  items_attributes: [{
+    pizza_id: Pizza.last.id,
+    size: :small
+  }]
+)
+
+order.save!
